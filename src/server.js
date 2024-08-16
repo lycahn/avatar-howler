d845e51b029d6c6e0bba7fe94698254dda6174b8
Szymon Wilczek <kazikwilczek7@gmail.com>
2024-08-16 19:17:39 +0200

    🪛 switching to vercel once again

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const AvatarComponent = ({ name, playgroundColors, size, square, variant, Avatar
 app.get('/avatar', async (req, res) => {
   const { generateUsername } = await import('unique-username-generator');
   const colors = (await import('nice-color-palettes/200')).default;
-  const { default: Avatar } = await import('../src/lib/components/Avatar');
+  const { default: Avatar } = await import('./lib/components/Avatar');
 
   const customPalette = colors[Math.floor(Math.random() * colors.length)];
   const variants = ['beam', 'bauhaus', 'ring'];
